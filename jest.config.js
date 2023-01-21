7f9328c2226b8bbf9bb5f6df7d2e89bb9efe842f
TheLokin <diego.ramil.lopez@gmail.com>
2023-01-21 12:21:13 +0100

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,9 @@ const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({ dir: "./renderer" });
 
-const customJestConfig = {
+module.exports = createJestConfig({
   clearMocks: true,
   testEnvironment: "jest-environment-jsdom",
   moduleDirectories: ["node_modules", "renderer"],
   setupFilesAfterEnv: ["./jest.setup.js"],
-};
-
-module.exports = createJestConfig(customJestConfig);
+});
