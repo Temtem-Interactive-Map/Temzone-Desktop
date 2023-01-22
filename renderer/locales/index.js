93b47e4e192aa4ca93afe65c53123546a1369ca6
TheLokin <diego.ramil.lopez@gmail.com>
2023-01-23 00:47:10 +0100

    Fix internationalization exception

--- renderer/locales/index.js
+++ renderer/locales/index.js
@@ -1,10 +1,14 @@
 import en from "locales/en.json";
 
-export function t(keys, ...params) {
+export function t(keys, params) {
   const key = keys.split(".").reduce((o, i) => o[i] ?? keys, en);
 
-  return Object.keys(params).reduce(
-    (o, i) => o.replace("@" + i, params[i]),
-    key
-  );
+  if (arguments.length === 1) {
+    return key;
+  } else {
+    return Object.keys(params).reduce(
+      (o, i) => o.replace("@" + i, params[i]),
+      key
+    );
+  }
 }
