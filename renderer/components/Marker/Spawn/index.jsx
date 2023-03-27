12f1dceac0db34d705dae273b84e435a3e0b7f2f
TheLokin <diego.ramil.lopez@gmail.com>
2023-03-27 18:48:35 +0200

    Update spawn marker component

--- renderer/components/Marker/Spawn/index.jsx
+++ renderer/components/Marker/Spawn/index.jsx
@@ -1,12 +1,9 @@
 import { ConditionField, LocationField, Marker } from "components/Marker";
-import { useTemzone } from "hooks/Temzone";
 import { t } from "locales";
 import { useCallback } from "react";
+import { updateSpawnMarker } from "services";
 
 export function SpawnMarker({ marker }) {
-  // State
-  const { updateSpawnMarker } = useTemzone();
-
   const handleMarkerUpdate = useCallback(
     (data, coordinates) => {
       const subtitle = data.location.trim();
@@ -21,7 +18,7 @@ export function SpawnMarker({ marker }) {
         marker.condition = condition;
       });
     },
-    [marker, updateSpawnMarker]
+    [marker]
   );
 
   return (
